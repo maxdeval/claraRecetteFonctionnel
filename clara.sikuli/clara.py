f7d098376566ff55f936a7a1339f0db0e1635ac8
maxdeval <maxime.devallez@pole-emploi.fr>
2018-05-22 16:25:29 +0200

    first green test

--- clara.sikuli/clara.py
+++ clara.sikuli/clara.py
@@ -4,10 +4,10 @@ def setUp(self):
 def tearDown(self):
     App.close("Google Chrome")
 
-def setUpClara():
+def testClara():
     click("1526994917663.png")
     paste("https://mae-attempt.herokuapp.com/")
-    type("Key.ENTER")
+    type(Key.ENTER)
     assert exists(Pattern("1526995285010.png").targetOffset(-26,100))
     
-    
+testClara()
