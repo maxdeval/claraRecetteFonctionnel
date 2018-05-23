9eae2b60c6a9242d929c770ff907d017b63d2a29
maxdeval <maxime.devallez@pole-emploi.fr>
2018-05-23 09:40:59 +0200

    format

--- clara.sikuli/clara.py
+++ clara.sikuli/clara.py
@@ -1,13 +1,20 @@
-def setUp(self):
-    App.open("Google Chrome")
+import unittest
 
-def tearDown(self):
-    App.close("Google Chrome")
+from sikuli.Sikuli import *
 
-def testClara():
-    click("1526994917663.png")
-    paste("https://mae-attempt.herokuapp.com/")
-    type(Key.ENTER)
-    assert exists(Pattern("1526995285010.png").targetOffset(-26,100))
-    
-testClara()
+
+class TestBasicScenario(unittest.TestCase):
+
+    def setUp():
+        App.open("Google Chrome")
+
+    def tearDown():
+        App.close("Google Chrome")
+
+    def test_01_open_clara():
+        click("1526994917663.png")
+        paste("https://mae-attempt.herokuapp.com/")
+        type(Key.ENTER)
+        assert exists(Pattern("1526995285010.png").targetOffset(-26,100))
+    setUp()    
+    test_01_open_clara()
