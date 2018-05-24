a78848b5bb1470434c73d036342e5f803eccce7a
maxdeval <maxime.devallez@pole-emploi.fr>
2018-05-24 12:55:17 +0200

    first green test - adresse question pop up

--- adresseQuestionPopup.sikuli/adresseQuestionPopup.py
+++ adresseQuestionPopup.sikuli/adresseQuestionPopup.py
@@ -6,13 +6,28 @@ class AdresseQuestionPopup(unittest.TestCase):
 
     def setUp(self):
         App.open("Firefox")
-    def tearDown(self):
-       App.close("Firefox")
-    def test_adresse_question_pop(self):
-        click("barre_de_recherche.png")
+#    def tearDown(self):
+#       App.close("Firefox")
+    def test_adresse_question_popup(self):
+        click("barre_recherche.png")
         paste("https://mae-attempt.herokuapp.com/")
         type(Key.ENTER)
         wait(4)
         click("1527087746609.png")
         click(Pattern("1527146499559.png").targetOffset(20,3))
-        click("1527147045965.png")
+        click("continuer.png")
+        click("assurance_chomage.png")
+        click("continuer.png")
+        click("montant_mois.png")
+        paste("300")
+        click("continuer.png")
+        wait(2)
+        paste("16")
+        click("continuer.png")
+        click("bac_3_licence.png")
+        click("continuer.png")
+        wait(2)
+        assert exists("adresse_question_popup.png")
+        
+        
+        
--- mainTest.sikuli/mainTest.py
+++ mainTest.sikuli/mainTest.py
@@ -1,9 +1,9 @@
 from sikuli.Sikuli import *
-from adresseQuestionPop import *
+from adresseQuestionPopup import *
 import unittest
 
 
-suite = unittest.TestLoader().loadTestsFromTestCase(TestBasicScenario)
+suite = unittest.TestLoader().loadTestsFromTestCase(AdresseQuestionPopup)
 unittest.TextTestRunner(verbosity=2).run(suite)
 
-test_adresse_question_pop(self):
+test_adresse_question_popup(self)
